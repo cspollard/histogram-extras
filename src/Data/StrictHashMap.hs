bc2d92a7e97d073f4c6fd2502be0ce8c22d08071
Chris Pollard <cspollard@gmail.com>
2021-06-13 11:11:43 +0100

    formatting

--- src/Data/StrictHashMap.hs
+++ src/Data/StrictHashMap.hs
@@ -1,43 +1,49 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE DeriveGeneric      #-}
-{-# LANGUAGE TemplateHaskell    #-}
-{-# LANGUAGE TypeFamilies       #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Data.StrictHashMap
-  ( StrictHashMap, strictHashMap
-  , intersectionWith, mapMaybeWithKey
-  ) where
+  ( StrictHashMap,
+    strictHashMap,
+    intersectionWith,
+    mapMaybeWithKey,
+  )
+where
 
-import Control.DeepSeq ( NFData )
+import Control.DeepSeq (NFData)
 import Control.Lens
-    ( Traversable(..),
-      Ixed(..),
-      Index,
-      IxValue,
-      (<&>),
-      view,
-      makePrisms,
-      At(..) )
-import Data.Align ( Align(..), Semialign(align) )
-import Data.Data ( Data, Typeable )
+  ( At (..),
+    Index,
+    IxValue,
+    Ixed (..),
+    Traversable (..),
+    makePrisms,
+    view,
+    (<&>),
+  )
+import Data.Align (Align (..), Semialign (align))
+import Data.Data (Data, Typeable)
 import Data.Functor.Classes
-    ( Show1(liftShowsPrec), showsUnaryWith, Show2(..) )
-import Data.Hashable ( Hashable )
-import qualified Data.HashMap.Strict  as HM
-import Data.Key ( FoldableWithKey(foldMapWithKey), Key, Keyed(..) )
-import Data.Serialize ( Serialize(..) )
-import           GHC.Exts             (IsList (..))
-import GHC.Generics ( Generic )
-import           Linear.Matrix        (Trace (..))
-import           Prelude              hiding (lookup)
-
+  ( Show1 (liftShowsPrec),
+    Show2 (..),
+    showsUnaryWith,
+  )
+import qualified Data.HashMap.Strict as HM
+import Data.Hashable (Hashable)
+import Data.Key (FoldableWithKey (foldMapWithKey), Key, Keyed (..))
+import Data.Serialize (Serialize (..))
+import GHC.Exts (IsList (..))
+import GHC.Generics (Generic)
+import Linear.Matrix (Trace (..))
+import Prelude hiding (lookup)
 
 newtype StrictHashMap k a = SHM (HM.HashMap k a)
   deriving (Show, Eq, Data, Typeable, Generic)
 
 makePrisms ''StrictHashMap
 
-instance (NFData k, NFData a) => NFData (StrictHashMap k a) where
+instance (NFData k, NFData a) => NFData (StrictHashMap k a)
 
 strictHashMap :: HM.HashMap k a -> StrictHashMap k a
 strictHashMap = SHM . force'
@@ -45,8 +51,10 @@ strictHashMap = SHM . force'
 force' :: HM.HashMap k a -> HM.HashMap k a
 force' m = HM.foldl' (flip seq) () m `seq` m
 
-instance (Ord k, Hashable k, Serialize k, Serialize a)
-  => Serialize (StrictHashMap k a) where
+instance
+  (Ord k, Hashable k, Serialize k, Serialize a) =>
+  Serialize (StrictHashMap k a)
+  where
   put = put . HM.toList . view _SHM
   get = SHM . HM.fromList <$> get
 
@@ -58,13 +66,15 @@ instance Keyed (StrictHashMap k) where
 instance FoldableWithKey (StrictHashMap k) where
   foldMapWithKey f = foldMapWithKey f . view _SHM
 
-
 mapMaybeWithKey :: (k -> a1 -> Maybe a) -> StrictHashMap k a1 -> StrictHashMap k a
 mapMaybeWithKey f (SHM m) = SHM $ HM.mapMaybeWithKey f m
 
-intersectionWith
-  :: (Hashable k, Ord k)
-  => (a1 -> b -> a) -> StrictHashMap k a1 -> StrictHashMap k b -> StrictHashMap k a
+intersectionWith ::
+  (Hashable k, Ord k) =>
+  (a1 -> b -> a) ->
+  StrictHashMap k a1 ->
+  StrictHashMap k b ->
+  StrictHashMap k a
 intersectionWith f (SHM m) (SHM m') = SHM $ HM.intersectionWith f m m'
 
 instance Functor (StrictHashMap k) where
@@ -93,29 +103,27 @@ instance (Hashable k, Ord k) => Monoid (StrictHashMap k a) where
   mappend = (<>)
 
 type instance Index (StrictHashMap k a) = k
-type instance IxValue (StrictHashMap k a) = a
 
+type instance IxValue (StrictHashMap k a) = a
 
 instance (Hashable k, Ord k) => At (StrictHashMap k a) where
   at k f (SHM m) =
     f mv <&> \r -> case r of
       Nothing -> maybe (SHM m) (const (SHM $ HM.delete k m)) mv
       Just v' -> SHM $ HM.insert k v' m
-    where mv = HM.lookup k m
-
+    where
+      mv = HM.lookup k m
 
 instance (Hashable k, Ord k) => Ixed (StrictHashMap k a) where
   ix k f (SHM m) =
     case HM.lookup k m of
-     Just v  -> f v <&> \v' -> SHM (HM.insert k v' m)
-     Nothing -> pure $ SHM m
-
+      Just v -> f v <&> \v' -> SHM (HM.insert k v' m)
+      Nothing -> pure $ SHM m
 
 instance (Hashable k, Ord k) => IsList (StrictHashMap k a) where
   type Item (StrictHashMap k a) = (k, a)
   fromList = strictHashMap . fromList
   toList = toList . view _SHM
-
 
 instance Show2 StrictHashMap where
   liftShowsPrec2 spk slk spv slv d (SHM m) =
